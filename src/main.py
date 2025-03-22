c477a80aea70985f88cb85ba3613482e5d6cbe3e
Andronov Vladislav <97717897+AndronovVladislav@users.noreply.github.com>
2025-03-23 01:13:29 +0300

    Add linter and healthcheck (#3)

--- src/main.py
+++ src/main.py
@@ -19,6 +19,12 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(wishlist_router)
 
+
+@app.get('/healthcheck')
+async def healthcheck():
+    return {'status': 'ok'}
+
+
 if __name__ == '__main__':
     uvicorn.run(app,
                 host=settings.uvicorn.host,
